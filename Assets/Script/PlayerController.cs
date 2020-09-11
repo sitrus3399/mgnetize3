048c3ce48ffa3d9d6633a1069edda6730a453af5
rehunter <hunter.kilua99@gmail.com>
2020-09-11 21:35:10 +0700

    no message

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    public GameObject startPosition;
+    public float moveSpeed = 5f;
+
+    //Variable Tower
+    public float pullForce = 100f;
+    public float rotateSpeed = 360f;
+    private GameObject closestTower;
+    private GameObject hookedTower;
+    private bool isPulled = false;
+    private UIControllerScript uiControl;
+
+    //Audio
+    private AudioSource myAudio;
+    private bool isCrashed = false;
+
+    void Start()
+    {
+        rb2D = this.gameObject.GetComponent<Rigidbody2D>();
+        uiControl = GameObject.Find("Canvas").GetComponent<UIControllerScript>();
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //Move the object
+        rb2D.velocity = -transform.up * moveSpeed;
+
+        if (Input.GetKey(KeyCode.Z) || Input.GetMouseButton(0) && !isPulled)
+        {
+            if(closestTower != null && hookedTower == null)
+            {
+                hookedTower = closestTower;
+            }
+
+            if(hookedTower)
+            {
+                float distance = Vector2.Distance(transform.position, hookedTower.transform.position);
+
+                //Gravitation toward tower
+                Vector3 pullDirection = (hookedTower.transform.position - transform.position).normalized;
+                float newPullForce = Mathf.Clamp(pullForce / distance, 20, 50);
+                rb2D.AddForce(pullDirection * newPullForce);
+
+                //Angular velocity
+                rb2D.angularVelocity = -rotateSpeed / distance;
+                isPulled = true;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z) || Input.GetMouseButtonUp(0))
+        {
+            isPulled = false;
+            hookedTower = null;
+            rb2D.velocity = -transform.up * moveSpeed;
+            rb2D.angularVelocity = 0f;
+        }
+
+        if (isCrashed)
+        {
+            if (!myAudio.isPlaying)
+            {
+                restartPosition();
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            if (!isCrashed)
+            {
+                //Play SFX
+                myAudio.Play();
+                rb2D.velocity = new Vector3(0f, 0f, 0f);
+                rb2D.angularVelocity = 0f;
+                isCrashed = true;
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Goal")
+        {
+            Debug.Log("Levelclear!");
+            uiControl.endGame();
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Tower")
+        {
+            closestTower = collision.gameObject;
+
+            //Changee tower color back to green as indicator
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isPulled) return;
+
+        if(collision.gameObject.tag == "Tower")
+        {
+            closestTower = null;
+            hookedTower = null;
+
+            //Change tower color back to normal
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+
+    public void restartPosition()
+    {
+        //Set to start position
+        this.transform.position = startPosition.transform.position;
+
+        //Restart rotation
+        this.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+
+        rb2D.velocity = -transform.up * moveSpeed;
+        rb2D.angularVelocity = 0f;
+        //Set isCrashed to false
+        isCrashed = false;
+
+        if (closestTower)
+        {
+            closestTower.GetComponent<SpriteRenderer>().color = Color.white;
+            closestTower = null;
+        }
+    }
+
+}
---
+++ Assets/Script/UIControllerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIControllerScript : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject resumeBtn;
+    public GameObject levelClearTxt;
+
+    private Scene currActiveScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currActiveScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseGame();
+        }
+    }
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void restartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currActiveScene.name);
+    }
+
+    public void endGame()
+    {
+        pausePanel.SetActive(true);
+        resumeBtn.SetActive(false);
+        levelClearTxt.SetActive(true);
+    }
+
+    public void scene1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void scene2()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
